8a5f7117e42948bbf919990b69e36d1c002ab999
Benjamin Pannell <benjamin@pannell.dev>
2019-04-08 22:11:41 -0700

    fix: Ensure that we include `.git` in git URLs for the big-3

--- Config.ps1
+++ Config.ps1
@@ -9,19 +9,19 @@ $GitTool = @{
             Name           = "github.com";
             NamespaceDepth = 1;
             WebURLFormat   = "https://github.com/{0}/{1}";
-            GitURLFormat   = "git@github.com:{0}/{1}";
+            GitURLFormat   = "git@github.com:{0}/{1}.git";
         };
         "bitbucket.org" = @{
             Name           = "bitbucket.org";
             NamespaceDepth = 1;
             WebURLFormat   = "https://bitbucket.org/{0}/{1}";
-            GitURLFormat   = "git@bitbucket.org:{0}/{1}";
+            GitURLFormat   = "git@bitbucket.org:{0}/{1}.git";
         };
         "gitlab.com"    = @{
             Name           = "gitlab.com";
             NamespaceDepth = 1;
             WebURLFormat   = "https://gitlab.com/{0}/{1}";
-            GitURLFormat   = "git@gitlab.com:{0}/{1}";
+            GitURLFormat   = "git@gitlab.com:{0}/{1}.git";
         };
         "dev.azure.com" = @{
             Name           = "dev.azure.com";
