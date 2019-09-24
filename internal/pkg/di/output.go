5cd8660427744b0815d6bc28e6d3aed1849c1b4f
Benjamin Pannell <benjamin@pannell.dev>
2019-09-24 10:51:44 +0100

    refactor: Add support for the io.StringWriter to the output abstraction
    This should hopefully fix UTF-8 printing issues.

--- internal/pkg/di/output.go
+++ internal/pkg/di/output.go
@@ -18,6 +18,7 @@ func SetOutput(o Output) {
 
 type Output interface {
 	io.Writer
+	io.StringWriter
 	Print(args ...interface{})
 	Println(args ...interface{})
 	Printf(format string, args ...interface{})
@@ -28,6 +29,10 @@ type StdOutput struct {
 
 func (o *StdOutput) Write(b []byte) (int, error) {
 	return os.Stdout.Write(b)
+}
+
+func (o *StdOutput) WriteString(s string) (int, error) {
+	return os.Stdout.WriteString(s)
 }
 
 func (o *StdOutput) Print(args ...interface{}) {
