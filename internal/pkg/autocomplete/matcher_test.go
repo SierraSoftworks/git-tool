ecb82e06f941958b51a1d6359433b1e0d44cb5d3
Benjamin Pannell <benjamin@pannell.dev>
2020-03-03 23:12:13 +0000

    test: Update autocomplete tests to use go test

--- internal/pkg/autocomplete/matcher_test.go
+++ internal/pkg/autocomplete/matcher_test.go
@@ -1,44 +1,27 @@
 package autocomplete_test
 
 import (
-	"fmt"
+	"testing"
 
 	"github.com/SierraSoftworks/git-tool/internal/pkg/autocomplete"
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("Matcher", func() {
-	Describe("Matches()", func() {
-		cases := []struct {
-			Value string
-			Seq   string
-			Match bool
-		}{
-			{"test", "t", true},
-			{"test", "x", false},
-			{"test", "te", true},
-			{"test", "tst", true},
-			{"test", "tsts", false},
-			{"test", "et", true},
-		}
+func TestMatcher(t *testing.T) {
+	cases := []struct {
+		Value string
+		Seq   string
+		Match bool
+	}{
+		{"test", "t", true},
+		{"test", "x", false},
+		{"test", "te", true},
+		{"test", "tst", true},
+		{"test", "tsts", false},
+		{"test", "et", true},
+	}
 
-		for _, tc := range cases {
-			val := tc.Value
-			seq := tc.Seq
-			expected := tc.Match
-
-			Context(fmt.Sprintf("Matches('%s', '%s') -> %v", tc.Value, tc.Seq, tc.Match), func() {
-				if expected {
-					It("Should match", func() {
-						Expect(autocomplete.Matches(val, seq)).To(BeTrue())
-					})
-				} else {
-					It("Should not match", func() {
-						Expect(autocomplete.Matches(val, seq)).To(BeFalse())
-					})
-				}
-			})
-		}
-	})
-})
+	for _, tc := range cases {
+		assert.Equalf(t, tc.Match, autocomplete.Matches(tc.Value, tc.Seq), "Matches('%s', '%s') should be %v", tc.Value, tc.Seq, tc.Match)
+	}
+}
