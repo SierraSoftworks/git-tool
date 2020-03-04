3220878e98eda0c156553797c424761eb586a8ca
Benjamin Pannell <benjamin@pannell.dev>
2020-03-04 15:50:47 +0000

    test: Migrate repo tests to use go test

--- internal/pkg/repo/mapper_test.go
+++ internal/pkg/repo/mapper_test.go
@@ -1,193 +1,116 @@
 package repo_test
 
 import (
-	"os"
+	"testing"
 
 	"github.com/SierraSoftworks/git-tool/internal/pkg/config"
 	"github.com/SierraSoftworks/git-tool/internal/pkg/di"
 	"github.com/SierraSoftworks/git-tool/internal/pkg/mocks"
 	"github.com/SierraSoftworks/git-tool/internal/pkg/repo"
-	"github.com/SierraSoftworks/git-tool/pkg/models"
 	"github.com/SierraSoftworks/git-tool/test"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("Mapper", func() {
-	var (
-		out    *mocks.Output
-		launch *mocks.Launcher
-		init   *mocks.Initializer
-		r      models.Repo
-		cfg    *mocks.Config
-		err    error
-	)
+func TestMapper(t *testing.T) {
+	cfg := mocks.NewConfig(config.DefaultForDirectory(test.GetTestPath("devdir")))
+	require.NotNil(t, cfg, "we should have a config")
+	out := &mocks.Output{}
+	launch := &mocks.Launcher{}
+	init := &mocks.Initializer{}
 
-	BeforeEach(func() {
-		cfg = mocks.NewConfig(config.DefaultForDirectory(test.GetTestPath("devdir")))
-		out = &mocks.Output{}
-		launch = &mocks.Launcher{}
-		init = &mocks.Initializer{}
-		di.SetOutput(out)
-		di.SetLauncher(launch)
-		di.SetInitializer(init)
-		di.SetMapper(&repo.Mapper{})
-	})
+	di.SetConfig(cfg)
+	di.SetOutput(out)
+	di.SetLauncher(launch)
+	di.SetInitializer(init)
+	di.SetMapper(&repo.Mapper{})
 
-	JustBeforeEach(func() {
-		di.SetConfig(cfg)
-	})
+	reset := func() {
+		cfg.Reset(config.DefaultForDirectory(test.GetTestPath("devdir")))
+		out.Reset()
+		launch.Reset()
+		init.Reset()
+	}
 
-	AfterEach(func() {
-		os.Chdir(test.GetProjectRoot())
-	})
+	t.Run("GetBestRepo()", func(t *testing.T) {
+		t.Run("Aliases", func(t *testing.T) {
+			reset()
+			cfg.AddAlias("alias1", "github.com/sierrasoftworks/test1")
+			cfg.AddAlias("alias2", "nonexistent.com/namespace/repo")
 
-	Describe("GetBestRepo()", func() {
-		Context("When an alias is provided", func() {
-			BeforeEach(func() {
-				cfg.AddAlias("alias1", "github.com/sierrasoftworks/test1")
-				cfg.AddAlias("alias2", "nonexistent.com/namespace/repo")
+			t.Run("Known Service", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("alias1")
+				require.NoError(t, err, "it should not return an error")
+				require.NotNil(t, repo, "it should not return a nil repo")
+
+				assert.Equal(t, "github.com", repo.Service().Domain(), "it should be the correct service")
+				assert.Equal(t, "sierrasoftworks/test1", repo.FullName(), "it should have the correct name")
 			})
 
-			Context("which matches a known repo", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("alias1")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should return the correct repo", func() {
-					Expect(r).ToNot(BeNil())
-					Expect(r.Service().Domain()).To(Equal("github.com"))
-					Expect(r.FullName()).To(Equal("sierrasoftworks/test1"))
-				})
+			t.Run("Unknown Service", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("alias2")
+				require.NoError(t, err, "it should not return an error")
+				assert.Nil(t, repo, "it should return a nil repo")
 			})
 
-			Context("which doesn't match a known service", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("alias2")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should not return a repo", func() {
-					Expect(r).To(BeNil())
-				})
-			})
-
-			Context("which hasn't been configured", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("alias3")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should not return a repo", func() {
-					Expect(r).To(BeNil())
-				})
+			t.Run("Unknown Alias", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("alias3")
+				require.NoError(t, err, "it should not return an error")
+				assert.Nil(t, repo, "it should return a nil repo")
 			})
 		})
 
-		Context("When a full repo name is provided", func() {
-			Context("with a service included", func() {
-				Context("which doesn't exist", func() {
-					JustBeforeEach(func() {
-						r, err = di.GetMapper().GetBestRepo("nonexistent.com/namespace/repo")
-					})
+		t.Run("Fully Qualified Names", func(t *testing.T) {
+			reset()
 
-					It("should not return an error", func() {
-						Expect(err).ToNot(HaveOccurred())
-					})
+			t.Run("Known Service", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("github.com/sierrasoftworks/test1")
+				require.NoError(t, err, "it should not return an error")
+				require.NotNil(t, repo, "it should not return a nil repo")
 
-					It("should not return a repo", func() {
-						Expect(r).To(BeNil())
-					})
-				})
-
-				Context("which does exist", func() {
-					JustBeforeEach(func() {
-						r, err = di.GetMapper().GetBestRepo("github.com/sierrasoftworks/test1")
-					})
-
-					It("should not return an error", func() {
-						Expect(err).ToNot(HaveOccurred())
-					})
-
-					It("should return the correct repo", func() {
-						Expect(r).ToNot(BeNil())
-						Expect(r.Service().Domain()).To(Equal("github.com"))
-						Expect(r.FullName()).To(Equal("sierrasoftworks/test1"))
-					})
-				})
+				assert.Equal(t, "github.com", repo.Service().Domain(), "it should be the correct service")
+				assert.Equal(t, "sierrasoftworks/test1", repo.FullName(), "it should have the correct name")
 			})
 
-			Context("without a service included", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("sierrasoftworks/test1")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should return the correct repo", func() {
-					Expect(r).ToNot(BeNil())
-					Expect(r.Service().Domain()).To(Equal("github.com"))
-					Expect(r.FullName()).To(Equal("sierrasoftworks/test1"))
-				})
+			t.Run("Unknown Service", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("nonexistent.com/namespace/repo")
+				require.NoError(t, err, "it should not return an error")
+				assert.Nil(t, repo, "it should return a nil repo")
 			})
 		})
 
-		Context("When a partial repo name is provided", func() {
-			Context("which matches a single repo", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("ghsstst1")
-				})
+		t.Run("Default Service Names", func(t *testing.T) {
+			reset()
 
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
+			repo, err := di.GetMapper().GetBestRepo("sierrasoftworks/test1")
+			require.NoError(t, err, "it should not return an error")
+			require.NotNil(t, repo, "it should not return a nil repo")
 
-				It("should return the correct repo", func() {
-					Expect(r).ToNot(BeNil())
-					Expect(r.Service().Domain()).To(Equal("github.com"))
-					Expect(r.FullName()).To(Equal("sierrasoftworks/test1"))
-				})
+			assert.Equal(t, "github.com", repo.Service().Domain(), "it should be the correct service")
+			assert.Equal(t, "sierrasoftworks/test1", repo.FullName(), "it should have the correct name")
+		})
+
+		t.Run("Partial Names", func(t *testing.T) {
+			t.Run("Single Match", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("ghsstst1")
+				require.NoError(t, err, "it should not return an error")
+				require.NotNil(t, repo, "it should not return a nil repo")
+
+				assert.Equal(t, "github.com", repo.Service().Domain(), "it should be the correct service")
+				assert.Equal(t, "sierrasoftworks/test1", repo.FullName(), "it should have the correct name")
 			})
 
-			Context("which matches more than one repo", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("test1")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should not return a repo", func() {
-					Expect(r).To(BeNil())
-				})
+			t.Run("Multiple Matches", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("test")
+				require.NoError(t, err, "it should not return an error")
+				assert.Nil(t, repo, "it should return a nil repo")
 			})
 
-			Context("which doesn't match a repo", func() {
-				JustBeforeEach(func() {
-					r, err = di.GetMapper().GetBestRepo("unrecognized")
-				})
-
-				It("should not return an error", func() {
-					Expect(err).ToNot(HaveOccurred())
-				})
-
-				It("should not return a repo", func() {
-					Expect(r).To(BeNil())
-				})
+			t.Run("No Matches", func(t *testing.T) {
+				repo, err := di.GetMapper().GetBestRepo("unrecognized")
+				require.NoError(t, err, "it should not return an error")
+				assert.Nil(t, repo, "it should return a nil repo")
 			})
 		})
 	})
-})
+}
