8288afef3851ebca7699516157f137ff291b6006
Benjamin Pannell <benjamin@pannell.dev>
2020-07-04 13:06:48 +0100

    test: Add some more tests for the updater public API

--- src/update/api.rs
+++ src/update/api.rs
@@ -99,4 +99,27 @@ mod tests {
             serde_json::from_str(r#"{"update":"/tmp/git-tool-update","phase":"cleanup"}"#).unwrap();
         assert_eq!(deserialized, update);
     }
+
+    #[test]
+    fn test_to_string() {
+        assert_eq!(UpdatePhase::Prepare.to_string(), "prepare");
+        assert_eq!(UpdatePhase::Replace.to_string(), "replace");
+        assert_eq!(UpdatePhase::Cleanup.to_string(), "cleanup");
+        assert_eq!(UpdatePhase::NoUpdate.to_string(), "no-update");
+    }
+
+    #[test]
+    fn test_for_phase() {
+        let update = UpdateState {
+            target_application: Some(PathBuf::from("/bin/git-tool")),
+            temporary_application: Some(PathBuf::from("/tmp/git-tool-update")),
+            phase: UpdatePhase::Replace
+        };
+
+        let new_update = update.for_phase(UpdatePhase::Cleanup);
+        assert_eq!(new_update.target_application, update.target_application);
+        assert_eq!(new_update.temporary_application, update.temporary_application);
+        assert_eq!(update.phase, UpdatePhase::Replace, "the old update entry should not be modified");
+        assert_eq!(new_update.phase, UpdatePhase::Cleanup, "the new update entry should have the correct phase");
+    }
 }
