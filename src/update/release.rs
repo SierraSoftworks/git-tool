8a7a19de95d0c15ca1f4fbef843f834e443aff53
Benjamin Pannell <benjamin@pannell.dev>
2020-07-04 12:58:58 +0100

    test: Add more tests for update release types

--- src/update/release.rs
+++ src/update/release.rs
@@ -6,14 +6,14 @@ pub struct Release {
     pub id: String,
     pub changelog: String,
     pub version: Version,
-    pub variants: Vec<ReleaseVariant>
+    pub variants: Vec<ReleaseVariant>,
 }
 
 impl Release {
     pub fn get_variant(&self, variant: &ReleaseVariant) -> Option<&ReleaseVariant> {
         for v in self.variants.iter() {
             if v == variant {
-                return Some(&v)
+                return Some(&v);
             }
         }
 
@@ -21,7 +21,8 @@ impl Release {
     }
 
     pub fn get_latest<'a, I>(releases: I) -> Option<&'a Self>
-    where I: IntoIterator<Item=&'a Self>
+    where
+        I: IntoIterator<Item = &'a Self>,
     {
         let mut latest: Option<&Self> = None;
 
@@ -47,7 +48,7 @@ impl PartialEq<Release> for Release {
 pub struct ReleaseVariant {
     pub id: String,
     pub arch: String,
-    pub platform: String
+    pub platform: String,
 }
 
 impl Default for ReleaseVariant {
@@ -55,7 +56,7 @@ impl Default for ReleaseVariant {
         Self {
             id: String::new(),
             platform: translate_platform(OS).to_string(),
-            arch: translate_arch(ARCH).to_string()
+            arch: translate_arch(ARCH).to_string(),
         }
     }
 }
@@ -69,7 +70,7 @@ impl PartialEq<ReleaseVariant> for ReleaseVariant {
 fn translate_platform(platform: &str) -> &str {
     match platform {
         "macos" => "darwin",
-        x => x
+        x => x,
     }
 }
 
@@ -77,7 +78,7 @@ fn translate_arch(arch: &str) -> &str {
     match arch {
         "x86_64" => "amd64",
         "i686" => "386",
-        x => x
+        x => x,
     }
 }
 
@@ -94,28 +95,94 @@ mod tests {
                 id: "1".to_string(),
                 changelog: "".to_string(),
                 version: "1.1.7".parse().unwrap(),
-                variants: vec![]
+                variants: vec![],
             },
             Release {
                 id: "0".to_string(),
                 changelog: "".to_string(),
                 version: "1.0.0".parse().unwrap(),
-                variants: vec![]
+                variants: vec![],
             },
             Release {
                 id: "3".to_string(),
                 changelog: "".to_string(),
                 version: "2.3.1".parse().unwrap(),
-                variants: vec![]
+                variants: vec![],
             },
             Release {
                 id: "2".to_string(),
                 changelog: "".to_string(),
                 version: "2.1.0".parse().unwrap(),
-                variants: vec![]
-            }
+                variants: vec![],
+            },
         ];
 
-        assert_eq!(Release::get_latest(releases.iter()).map(|r| r.id.as_str()), Some("3"));
+        assert_eq!(
+            Release::get_latest(releases.iter()).map(|r| r.id.as_str()),
+            Some("3")
+        );
+    }
+
+    #[test]
+    fn test_variant_equality() {
+        let v1 = ReleaseVariant {
+            id: "test1".to_string(),
+            arch: "x86_64".to_string(),
+            platform: "linux".to_string(),
+        };
+
+        let v2 = ReleaseVariant {
+            id: "test2".to_string(),
+            arch: "x86_64".to_string(),
+            platform: "linux".to_string(),
+        };
+
+        let v3 = ReleaseVariant {
+            id: "test3".to_string(),
+            arch: "x86_64".to_string(),
+            platform: "windows".to_string(),
+        };
+
+        assert_eq!(v1, v2);
+        assert_ne!(v1, v3);
+        assert_ne!(v2, v3);
+    }
+
+    #[test]
+    fn test_get_variants() {
+        let release = Release {
+            id: "test".to_string(),
+            changelog: "...".to_string(),
+            version: "1.0.0".parse().unwrap(),
+            variants: vec![
+                ReleaseVariant {
+                    id: "windows-x64".to_string(),
+                    arch: "x86_64".to_string(),
+                    platform: "windows".to_string(),
+                },
+                ReleaseVariant {
+                    id: "linux-x64".to_string(),
+                    arch: "x86_64".to_string(),
+                    platform: "linux".to_string(),
+                },
+            ],
+        };
+
+        let v1 = ReleaseVariant {
+            id: "test1".to_string(),
+            arch: "x86_64".to_string(),
+            platform: "linux".to_string(),
+        };
+
+        let v = release.get_variant(&v1);
+        assert_eq!(
+            v,
+            Some(&ReleaseVariant {
+                id: "linux-x64".to_string(),
+                arch: "x86_64".to_string(),
+                platform: "linux".to_string()
+            })
+        );
+        assert_eq!(v.unwrap().id, "linux-x64");
     }
 }
