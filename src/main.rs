2da6c98a30337b2d2438e4067921bd4196ccfc53
Benjamin Pannell <benjamin@pannell.dev>
2020-07-10 11:29:46 +0100

    fix: Ensure that we submit crash information before we exit the application

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ mod test;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let _guard = sentry::init((
+    let raven = sentry::init((
         "https://0787127414b24323be5a3d34767cb9b8@o219072.ingest.sentry.io/1486938",
         sentry::ClientOptions {
             release: sentry::release_name!(),
@@ -72,6 +72,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     match run(&mut app, commands, matches).await {
         Result::Ok(status) => {
+            raven.close(None);
             std::process::exit(status);
         }
         Result::Err(err) => {
@@ -82,6 +83,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 sentry::capture_error(&err);
             }
 
+            raven.close(None);
             std::process::exit(1);
         }
     }
