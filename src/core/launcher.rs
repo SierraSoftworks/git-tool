134c84f1967ab92cf138d034241bdcd1fcb48abe
Benjamin Pannell <benjamin@pannell.dev>
2020-12-31 10:36:39 +0000

    fix: Address issues with Unix compilation of the launcher for Tokio 1.0

--- src/core/launcher.rs
+++ src/core/launcher.rs
@@ -74,18 +74,20 @@ impl TokioLauncher {
 
     #[cfg(unix)]
     async fn forward_signals(&self, child: &mut tokio::process::Child) -> Result<i32, Error> {
-        let pid = nix::unistd::Pid::from_raw(child.id().try_into().map_err(|err| crate::errors::system_with_internal(
+        let child_id = child.id().ok_or(crate::errors::user(
+            "Unable to determine the child process's PID because the child process has already exited.",
+            "This might not be a problem, depending on the program you are running, however it may also indicate that the process is not running correctly."
+        ))?;
+
+        let pid = nix::unistd::Pid::from_raw(child_id.try_into().map_err(|err| crate::errors::system_with_internal(
             "Unable to convert child process ID to a valid PID. This may impact Git-Tool's ability to forward signals to a child application.",
             "Please report this error to us on GitHub, along with information about your operating system and version of Git-Tool, so that we can investigate further.",
             err))?);
-        let fused_child = child.fuse();
 
         let mut sigint = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())?;
         let mut sigterm =
             tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
         let mut sigquit = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::quit())?;
-
-        pin_mut!(fused_child);
 
         loop {
             let sigint = sigint.recv().fuse();
@@ -94,7 +96,7 @@ impl TokioLauncher {
 
             pin_mut!(sigint, sigterm, sigquit);
 
-            select! {
+            tokio::select! {
                 _ = sigint => {
                     nix::sys::signal::kill(pid, nix::sys::signal::Signal::SIGINT)?;
                 },
@@ -104,7 +106,7 @@ impl TokioLauncher {
                 _ = sigquit => {
                     nix::sys::signal::kill(pid, nix::sys::signal::Signal::SIGQUIT)?;
                 },
-                status = fused_child => {
+                status = child.wait() => {
                     return Ok(status?.code().unwrap_or_default())
                 }
             }
