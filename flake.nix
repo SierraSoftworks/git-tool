02c7d7a918989f5cb302d159589f1485b703fbd2
Benjamin Pannell <benjamin@pannell.dev>
2022-11-09 22:47:30 +0000

    feat: Add initial flake support

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "Simplify checking out your Git repositories in a structured directory space.";
+
+  outputs = { self, nixpkgs }:
+    {
+      devShells = nixpkgs.lib.genAttrs ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"] (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = pkgs.lib;
+          stdenv = pkgs.stdenv;
+        in
+        {
+          default = stdenv.mkDerivation {
+            name = "git-tool-shell-${system}";
+            system = system;
+            nativeBuildInputs = [
+              pkgs.rustc
+              pkgs.cargo
+              pkgs.nodejs
+              pkgs.protobuf
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              pkgs.libiconv
+              pkgs.openssl
+            ]
+              ++ lib.optionals stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.Security ]
+              ++ lib.optionals stdenv.isLinux [ pkgs.dbus ];
+
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+          };
+        }
+      );
+
+      packages = nixpkgs.lib.genAttrs ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"] (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = pkgs.lib;
+          rustPlatform = pkgs.rustPlatform;
+          nodePackages = pkgs.nodePackages;
+        in
+        {
+          git-tool = rustPlatform.buildRustPackage rec {
+            name = "git-tool-${system}";
+            pname = "git-tool";
+
+            src = pkgs.nix-gitignore.gitignoreSourcePure ''
+            /example
+            /target
+            /result
+            *.nix
+            '' ./.;
+
+            doCheck = false;
+
+            nativeBuildInputs = [
+              pkgs.protobuf
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              pkgs.libiconv
+              pkgs.openssl
+            ]
+              ++ lib.optionals pkgs.stdenv.isDarwin [pkgs.darwin.apple_sdk.frameworks.Security]
+              ++ lib.optionals stdenv.isLinux [ pkgs.dbus ];
+
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+
+              outputHashes = {
+                  "tracing-0.2.0" = "sha256-xK2F6TNne+scfKgU4Vr1tfe0kdXyOZt0N7bex0Jzcmg=";
+              };
+            };
+
+            meta = with lib; {
+              description = "Simplify checking out your Git repositories in a structured directory space.";
+              homepage = "https://git-tool.sierrasoftworks.com";
+              license = licenses.mit;
+              maintainers = [
+                {
+                  name = "Benjamin Pannell";
+                  email = "contact@sierrasoftworks.com";
+                }
+              ];
+            };
+          };
+          default = self.packages.${system}.git-tool;
+        }
+      );
+    };
+}
