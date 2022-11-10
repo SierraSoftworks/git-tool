7e25b7f94186eca962c274f19886cd4bc8cfc781
Benjamin Pannell <benjamin@pannell.dev>
2022-11-10 13:00:45 +0000

    fix: Resolve issues saving config using gt config add when the file path doesn't exist

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -127,11 +127,7 @@ impl CommandRunnable for ConfigCommand {
 
                 match cfg.get_config_file() {
                     Some(path) => {
-                        tokio::fs::write(&path, cfg.to_string()?).await.map_err(|err| errors::user_with_internal(
-                            &format!("Could not write your updated config to the config file '{}' due to an OS-level error.", path.display()),
-                            "Make sure that Git-Tool has permission to write to your config file and then try again.",
-                            err
-                        ))?;
+                        cfg.save(&path).await?;
                     }
                     None => {
                         writeln!(output, "{}", cfg.to_string()?)?;
