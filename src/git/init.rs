ac74416b92ef1b618ff1bc353d08c5fea671231b
Benjamin Pannell <benjamin@pannell.dev>
2025-03-27 17:29:11 +0000

    test: Add tests for git init command

--- src/git/init.rs
+++ src/git/init.rs
@@ -10,3 +10,22 @@ pub async fn git_init(path: &path::Path) -> Result<(), errors::Error> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_git_init() {
+        let temp_dir = tempfile::tempdir().expect("a temporary directory");
+
+        git_init(temp_dir.path())
+            .await
+            .expect("git init to succeed");
+
+        assert!(
+            temp_dir.path().join(".git").exists(),
+            "git init to create .git directory"
+        );
+    }
+}
