427c424e35cfa360e6f18d9ee9b1733b07f9930d
Benjamin Pannell <benjamin@pannell.dev>
2026-05-25 10:58:39 +0100

    fix: Don't remove azurerm provider yet

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,6 +2,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.74.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.8.0"
+    }
+
     honeycombio = {
       source  = "honeycombio/honeycombio"
       version = "~> 0.49.0"
@@ -25,3 +35,26 @@ terraform {
 }
 
 provider "cloudflare" {}
+
+
+
+provider "azurerm" {
+  features {}
+
+  // NOTE: You can retrieve this secret using `op read op://epfkgzb2bz4msye2xrhffiz3se/jrlwg64m56hkbkbfvgljfkwcfy/Azure/client_secret`
+  subscription_id = var.azure_subscription
+  tenant_id       = var.azure_tenant
+}
+
+provider "azuread" {
+  // NOTE: You can retrieve this secret using `op read op://epfkgzb2bz4msye2xrhffiz3se/jrlwg64m56hkbkbfvgljfkwcfy/Azure/client_secret`
+  tenant_id = var.azure_tenant
+}
+
+variable "azure_subscription" {
+
+}
+
+variable "azure_tenant" {
+
+}
